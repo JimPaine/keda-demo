c037224e53df753c99b7d24d9eca5d0c2f162529
Jim Paine <jim@jimpaine.io>
2019-05-17 15:59:18 +0100

    Added functions and build for http

--- env/outputs.tf
+++ env/outputs.tf
@@ -1,3 +1,15 @@
+output "kubeconfig" {
+    value = "${azurerm_kubernetes_cluster.keda.kube_config}"
+}
 
+output "acr_server" {
+    value = "${azurerm_container_registry.keda.login_server}"
+}
 
+output "acr_password" {
+    value = "${azurerm_container_registry.keda.admin_password}"
+}
 
+output "acr_user" {
+    value = "${azurerm_container_registry.keda.admin_username}"
+}
